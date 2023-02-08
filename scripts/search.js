ecb788a86dec95cf57d595f4de217c157cbd2dd8
Radu <nicuflorinradu308@gmail.com>
2023-02-08 14:52:37 +0100

    message

--- scripts/search.js
+++ scripts/search.js
@@ -1,33 +1,29 @@
-
-
-
-var TennisKeywords = ["t", "te", "ten", "tenn", "tenni", "tennis", "tennis game"];
-var LudoKeywords = ["l", "lu", "lud", "ludo", "ludo game"];
-var SnakeKeywords = ["s", "sn", "sna", "snak", "snake", "snake game"];
-var QuizKeywords = ["q", "qu", "qui", "quiz", "quick", "quick quiz", "quiz game"];
-var TetrisKeywords = ["t", "te", "tet", "tetr", "tetris", "tetris game"];
-var FishKeywords = ["f", "fi", "fis", "fish", "fish game"];
-var ReaganKeywords = ["r", "re", "rea", "reag", "reaga", "reagan", "reagan game"];
+var FishKeywords = ["f", "fi", "fis", "fish"];
+var ReaganKeywords = ["r", "re", "rea", "reag", "reaga", "reagan"];
 var MazeKeywords = ["m", "ma", "maz", "maze"];
+var ProjectKeywords = ["p", "pr", "pro", "proj", "proje", "projec", "project"];
+var TetrissKeywords = ["t", "te", "tet", "tetr", "tetris"];
+var PingKeywords = ["p", "pi", "pin", "ping", "pingp"];
+
+
 
 //didn't used these varaible as we get a error if just use these
-var TennisCard = document.getElementById("tennisgame");
-var LudoCard = document.getElementById("ludogame");
-var SnakeCard = document.getElementById("snakegame");
-var QuizCard = document.getElementById("quizgame");
-var TetrisCard = document.getElementById("tetrisgame");
 var FishCard = document.getElementById("fishgame");
 var ReaganCard = document.getElementById("reagangame");
 var MazeCard = document.getElementById("mazegame");
+var ProjectCard = document.getElementById("project");
+var TetrissCard = document.getElementById("tetris");
+var PingCard = document.getElementById("pingpong");
 
-var itsTennis = false;
-var itsLudo = false;
-var itsSnake = false;
-var itsTetris = false;
-var itsQuiz = false;
+
 var itsFish = false;
 var itsReagan = false;
 var itsMaze = false;
+var itsProject = false;
+var itsTetriss = false;
+var itsPing = false;
+
+
 
 
 //document.getElementById("searchbar").addEventListener("change", Search());
@@ -36,16 +32,12 @@ var itsMaze = false;
 
 
 function Reset() {
-	document.getElementById("ludogame")?.style.display = 'block';
-	document.getElementById("snakegame")?.style.display = 'block';
-	document.getElementById("tennisgame")?.style.display = 'block';
-	document.getElementById("tetrisgame")?.style.display = 'block';
-	document.getElementById("quizgame")?.style.display = 'block';
-	document.getElementById("fishgame")?.style.display = 'block';
-	document.getElementById("reagangame")?.style.display = 'block';
-	document.getElementById("mazegame")?.style.display = 'block';
-
-
+	document.getElementById("fishgame").style.display = 'block';
+	document.getElementById("reagangame").style.display = 'block';
+	document.getElementById("mazegame").style.display = 'block';
+	document.getElementById("project").style.display = 'block';
+	document.getElementById("tetris").style.display = 'block';
+	document.getElementById("pingpong").style.display = 'block';
 
 };
 
@@ -57,59 +49,48 @@ function Search() {
 	Reset();
 
 
-	if (TennisKeywords.includes(SearchValue) == true) {
-		itsTennis = true;
-	}
-
-	if (LudoKeywords.includes(SearchValue) == true) {
-		itsLudo = true;
-	}
-
-	if (SnakeKeywords.includes(SearchValue) == true) {
-		itsSnake = true;
-	}
-
-	if (TetrisKeywords.includes(SearchValue) == true) {
-		itsTetris = true;
-	}
-
-	if (QuizKeywords.includes(SearchValue) == true) {
-		itsQuiz = true;
-	}
-
 	if (FishKeywords.includes(SearchValue) == true) {
-		itsQuiz = true;
+		itsFish = true;
 	}
 
 	if (ReaganKeywords.includes(SearchValue) == true) {
-		itsQuiz = true;
+		itsReagan = true;
 	}
 
 	if (MazeKeywords.includes(SearchValue) == true) {
-
+		itsMaze = true;
 	}
+
+	if (TetrissKeywords.includes(SearchValue) == true) {
+		itsTetriss = true;
+	}
+
+	if (ProjectKeywords.includes(SearchValue) == true) {
+		itsProject = true;
+	}
+
+	if (PingKeywords.includes(SearchValue) == true) {
+		itsPing = true;
+	}
+
+
+
 
 
 	//this statement for the case if entered value  does not match
 
-	if (TennisKeywords.includes(SearchValue) == false) {
-		if (LudoKeywords.includes(SearchValue) == false) {
-			if (SnakeKeywords.includes(SearchValue) == false) {
-				if (TetrisKeywords.includes(SearchValue) == false) {
-					if (QuizKeywords.includes(SearchValue) == false) {
-						if (FishKeywords.includes(SearchValue) == false) {
-							if (ReaganKeywords.includes(SearchValue) == false) {
-								if (MazeKeywords.includes(SearchValue) == false) {
-									document.getElementById("ludogame").style.display = 'none';
-									document.getElementById("snakegame").style.display = 'none';
-									document.getElementById("tennisgame").style.display = 'none';
-									document.getElementById("tetrisgame").style.display = 'none';
-									document.getElementById("quizgame").style.display = 'none';
-									document.getElementById("fishgame").style.display = 'none';
-									document.getElementById("reagangame").style.display = 'none';
-									document.getElementById("mazegame").style.display = 'none';
-								}
-							}
+	if (FishKeywords.includes(SearchValue) == false) {
+		if (ReaganKeywords.includes(SearchValue) == false) {
+			if (MazeKeywords.includes(SearchValue) == false) {
+				if (TetrissKeywords.includes(SearchValue) == false) {
+					if (ProjectKeywords.includes(SearchValue) == false) {
+						if (PingKeywords.includes(SearchValue) == false) {
+							document.getElementById("fishgame").style.display = 'none';
+							document.getElementById("reagangame").style.display = 'none';
+							document.getElementById("mazegame").style.display = 'none';
+							document.getElementById("tetris").style.display = 'none';
+							document.getElementById("project").style.display = 'none';
+							document.getElementById("pinpong").style.display = 'none';
 						}
 					}
 				}
@@ -118,159 +99,124 @@ function Search() {
 	}
 
 
-	if (itsTennis == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		itsTennis = false;
-	}
 
-	if (itsLudo == true) {
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		itsLudo = false;
-	}
-
-	if (itsSnake == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-	}
-
-	if (itsTetris == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		itsTetris = false;
-	}
-
-	if (itsQuiz == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		itsQuiz = false;
-	}
 
 	if (itsFish == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
 		itsFish = false;
 	}
 
 	if (itsReagan == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
 		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
 		itsReagan = false;
 	}
 
 	if (itsMaze == true) {
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("fishgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
 		itsMaze = false;
 	}
 
+	if (itsTetriss == true) {
+		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
+		itsTetriss = false;
+	}
 
-	//Using this statement as we have t and te in two of the games name
-	if ((SearchValue == 't') || (SearchValue == 'te')) {
-		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
+	if (itsProject == true) {
 		document.getElementById("fishgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
 		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
+		itsProject = false;
 	}
 
+	if (itsPing == true) {
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		itsPing = false;
+	}
+
+
+
+
+	//Using this statement as we have t and te in two of the games name
 	if ((SearchValue == 'f') || (SearchValue == 'fi') || (SearchValue == 'fis') || (SearchValue == 'fish')) {
 		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
 	}
 
 	if ((SearchValue == 'r') || (SearchValue == 're') || (SearchValue == 'rea') || (SearchValue == 'reag') || (SearchValue == 'reaga') || (SearchValue == 'reagan')) {
 		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
 		document.getElementById("fishgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
+
 	}
+
 
 	if ((SearchValue == 'm') || (SearchValue == 'ma') || (SearchValue == 'maz') || (SearchValue == 'maze')) {
 		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("fishgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
+
 	}
 
-	if ((SearchValue == 's') || (SearchValue == 'sn') || (SearchValue == 'sna') || (SearchValue == 'snak') || (SearchValue == 'snake')) {
+	if ((SearchValue == 'p') || (SearchValue == 'pr') || (SearchValue == 'pro') || (SearchValue == 'proj')) {
 		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("mazegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("fishgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
 	}
 
-	if ((SearchValue == 'q') || (SearchValue == 'qu') || (SearchValue == 'qui') || (SearchValue == 'quiz')) {
+	if ((SearchValue == 't') || (SearchValue == 'te') || (SearchValue == 'tet') || (SearchValue == 'tetr')) {
 		Reset();
-		document.getElementById("ludogame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("mazegame").style.display = 'none';
-		document.getElementById("fishgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("pingpong").style.display = 'none';
 	}
 
-	if ((SearchValue == 'l') || (SearchValue == 'lu') || (SearchValue == 'lud') || (SearchValue == 'ludo')) {
+	if ((SearchValue == 'p') || (SearchValue == 'pi') || (SearchValue == 'pin') || (SearchValue == 'ping')) {
 		Reset();
-		document.getElementById("mazegame").style.display = 'none';
-		document.getElementById("snakegame").style.display = 'none';
-		document.getElementById("quizgame").style.display = 'none';
-		document.getElementById("fishgame").style.display = 'none';
-		document.getElementById("tennisgame").style.display = 'none';
-		document.getElementById("tetrisgame").style.display = 'none';
 		document.getElementById("reagangame").style.display = 'none';
+		document.getElementById("fishgame").style.display = 'none';
+		document.getElementById("mazegame").style.display = 'none';
+		document.getElementById("project").style.display = 'none';
+		document.getElementById("tetris").style.display = 'none';
 	}
-
 
 	if (SearchValue == '') {
 		Reset();
 	}
-
-
 }
-
